31951503042ec986c051776751ef278a91dd225f
irinashemonaeva <irishemonaeva@gmail.com>
2024-05-27 00:03:44 +0300

    2nd try

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-
+    public static String[] Roman = new String[] {
+            null, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+            "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+            "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+    };
 
     public static void main(String[] args) throws Exception{
-        int num1, num2, answer, type; // type: 0 - arabic, 1 - roman
+        Main calculator = new Main();
+        calculator.runCalculator();
+    }
+
+    public void runCalculator() throws Exception {
+        int num1, num2, answer;
         String operator;
-        num1 = 0;
         //считываем информацию
         Scanner myObj = new Scanner(System.in);
         String input = myObj.nextLine();
-        String[] Roman = new String[] {null, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
         String[] parts = input.split(" ");
         operator = parts[1];
-        boolean isRoman1, isRoman2;
         if (parts.length>3) {
             throw new Exception("Неверный формат ввода");
         }
-        List<String> romanList = Arrays.asList(Roman);
-        num1 = romanList.indexOf(parts[0]);
-        num2 = romanList.indexOf(parts[2]);
-        if ((num1>0)&&(num2>0)&&(num1<=10)&&(num2<=10)) {
-            if (operator.equals("+")) {
-                answer = num1 + num2;
-                System.out.println(Roman[answer]);
-            }
-            else if (operator.equals("-")) {
-                answer = num1 - num2;
-                System.out.println(Roman[answer]);
-            }
-            else if (operator.equals("*")) {
-                answer = num1 * num2;
-                System.out.println(Roman[answer]);
-            }
-            else if (operator.equals("/")) {
-                answer = num1 / num2;
-                System.out.println(Roman[answer]);
-            }
+        boolean isRomanInput1 = isRoman(parts[0]);
+        boolean isRomanInput2 = isRoman(parts[2]);
+        if (isRomanInput1&&isRomanInput2) {
+            num1 = romanToArabic(parts[0]);
+            num2 = romanToArabic(parts[2]);
         } else {
-            num1 = Integer.valueOf(parts[0]);
-            num2 = Integer.valueOf(parts[2]);
-            if ((num1>10)|(num2>10)) {
-                throw new Exception("Введите числа меньше 10.");
-            }
-            else if (operator.equals("+")) {
-                answer = num1 + num2;
-            }
-            else if (operator.equals("-")) {
-                answer = num1 - num2;
-            }
-            else if (operator.equals("*")) {
-                answer = num1 * num2;
-            }
-            else if (operator.equals("/")) {
-                answer = num1 / num2;
-            }
-            else {
-                throw new Exception("Неверный формат ввода");
-            }
+            num1 = Integer.parseInt(parts[0]);
+            num2 = Integer.parseInt(parts[2]);
+        }
+        if ((num1>10)||(num2>10)) {
+            throw new Exception("Введите числа меньше или равные 10");
+        }
+        answer = calculate(num1, num2, parts[1], isRomanInput1);
+
+        if (isRomanInput1) {
+            System.out.println(arabicToRoman(answer));
+        } else {
             System.out.println(answer);
         }
-
     }
+
+    private boolean isRoman(String input) {
+        List<String> romanList = Arrays.asList(Roman);
+        return romanList.contains(input);
+    }
+
+    private int romanToArabic(String roman) {
+        return Arrays.asList(Roman).indexOf(roman);
+    }
+    private int calculate(int num1, int num2, String operator, boolean isRoman) throws Exception {
+        int answer;
+        if (operator.equals("+")) {
+            answer = num1 + num2;
+        }
+        else if (operator.equals("-")) {
+            answer = num1 - num2;
+        }
+        else if (operator.equals("*")) {
+            answer = num1 * num2;
+        }
+        else if (operator.equals("/")) {
+            answer = num1 / num2;
+        }
+        else {
+            throw new Exception("Неверный формат ввода");
+        }
+        return answer;
+    }
+
+    private String arabicToRoman(int answer) {
+        return Roman[answer];
+    }
+
+
 }
